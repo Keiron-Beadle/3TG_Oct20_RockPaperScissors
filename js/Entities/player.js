3a596fc7fb059975c40f26397dff07bc68bb8dec
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-10-31 16:46:45 +0000

    player newPosition return transform matrix

--- js/Entities/player.js
+++ js/Entities/player.js
@@ -18,39 +18,72 @@ class Player {
      }
  
      draw(pWorldMatrix){
-         this.mAnimatedSpriteSheet.draw(pWorldMatrix);
+        var transformMatrix = this.newPosition(pWorldMatrix);
+        this.mAnimatedSpriteSheet.draw(transformMatrix);
     }   
 
-    newPosition() {
-        this.mPosition.getX() += this.mSpeedX;
-        this.mPosition.getY() += this.mSpeedY;
-      }
+    newPosition(pWorldMatrix) {
+        var x, y, translate, transform, newPosition;
+
+        x = this.mPosition.getX();
+        x += this.mSpeedX;
+        y = this.mPosition.getY();
+        y += this.mSpeedY;
+
+        newPosition = new Vector(x, y, 1);
+
+        translate = Matrix.createTranslation(newPosition);
+        transform = pWorldMatrix.multiply(translate);
+        
+        return transform;
+    }
     
+    /*
     updateGameArea() {
-      myGameArea.clear();
+      //myGameArea.clear();
       this.mAnimatedSpriteSheet.newPos();
       this.mAnimatedSpriteSheet.update();
     }
-    
+    */
+
     moveup() {
-        this.mAnimatedSpriteSheet.mSpeedY -= 1;
+        this.mSpeedY -= 1;
     }
     
     movedown() {
-        this.mAnimatedSpriteSheet.mSpeedY += 1;
+        this.mSpeedY += 1;
     }
     
     moveleft() {
-        this.mAnimatedSpriteSheet.mSpeedX -= 1;
+        this.mSpeedX -= 1;
     }
     
     moveright() {
-        this.mAnimatedSpriteSheet.mSpeedX += 1;
+        this.mSpeedX += 1;
     }
 
     stopMove() {
-        this.mAnimatedSpriteSheet.speedX = 0;
-        this.mAnimatedSpriteSheet.speedY = 0;
+        this.mSpeedX = 0;
+        this.mSpeedY = 0;
+    }
+
+    setupKeyControls() {
+        document.onkeydown = function(e) {
+          switch (e.keyCode) {
+            case 37:
+            cube.rotation.x += 1;
+            break;
+            case 38:
+            cube.rotation.z -= 1;
+            break;
+            case 39:
+            cube.rotation.x -= 1;
+            break;
+            case 40:
+            cube.rotation.z += 1;
+            break;
+          }
+        };
     }
 }
     
